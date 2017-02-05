7f626cdfa6431f2a7a0b17350f84104fb3fb8c61
William Huang <huang.william.cn@hotmail.com>
2017-02-05 14:23:58 -0500

    Fixed OI file issues.

--- src/OI.cpp
+++ src/OI.cpp
@@ -1,8 +1,30 @@
 #include "OI.h"
 
-#include <WPILib.h>
+//#include <WPILib.h>
 
 OI::OI() {
 	// Process operator interface input here.
+	driver = new Joystick(0);
+	operatorJoy = new Joystick(1);
 
+	//
+
+	driverX = new JoystickButton(driver, 0);
+	driverA = new JoystickButton(driver, 1);
+	driverB = new JoystickButton(driver, 2);
+	driverY = new JoystickButton(driver, 3);
+	driverLB = new JoystickButton(driver, 4);
+	driverRB = new JoystickButton(driver, 5);
+	driverLT = new JoystickButton(driver, 6);
+	driverRT = new JoystickButton(driver, 7);
+
+	operatorX = new JoystickButton(operatorJoy, 0);
+	operatorA = new JoystickButton(operatorJoy, 1);
+	operatorB = new JoystickButton(operatorJoy, 2);
+	operatorY = new JoystickButton(operatorJoy, 3);
+
+	operatorLB = new JoystickButton(operatorJoy, 4);
+	operatorRB = new JoystickButton(operatorJoy, 5);
+	operatorLT = new JoystickButton(operatorJoy, 6);
+	operatorRT = new JoystickButton(operatorJoy, 7);
 }
--- src/OI.h
+++ src/OI.h
@@ -1,8 +1,39 @@
 #ifndef OI_H
 #define OI_H
 
+
+#include <WPILib.h>
+
 class OI {
 public:
+
+	static const Joystick driver;
+	static const Joystick operatorJoy;
+
+	static const JoystickButton* driverX;
+	static const JoystickButton* driverA;
+	static const JoystickButton* driverB;
+	static const JoystickButton* driverY;
+
+	static const JoystickButton* driverLB;
+	static const JoystickButton* driverRB;
+	static const JoystickButton* driverLT;
+	static const JoystickButton* driverRT;
+
+	static const JoystickButton* operatorX;
+	static const JoystickButton* operatorA;
+	static const JoystickButton* operatorB;
+	static const JoystickButton* operatorY;
+
+	static const JoystickButton* operatorLB;
+	static const JoystickButton* operatorRB;
+	static const JoystickButton* operatorLT;
+	static const JoystickButton* operatorRT;
+
+//DRIVE CODE
+
+//	JoystickTrigger joystickTrigger = JoystickTrigger();
+
 	OI();
 };
 
