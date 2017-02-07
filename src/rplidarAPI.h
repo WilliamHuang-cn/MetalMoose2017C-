a697c8e194d1369726ac480bca586e9857392641
William Huang <huang.william.cn@hotmail.com>
2017-02-07 17:11:30 -0500

    RPLidar API files added.

---
+++ src/rplidarAPI.h
@@ -0,0 +1,33 @@
+/*
+ * rplidarAPI.h
+ *
+ *  Created on:
+ *      Author: charles
+ */
+#include <iostream>
+#include "rplidar.h"
+
+#ifndef RPLIDARAPI_H_
+#define RPLIDARAPI_H_
+
+#endif /* RPLIDARAPI_H_ */
+
+using namespace rp::standalone::rplidar;
+
+#ifndef _countof
+#define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
+#endif
+
+typedef struct _rplidar_date_t {
+	bool	sync;
+    float   theta;
+    float 	distance;
+    int		quality;
+} rplidar_date_t;
+
+
+//opt_com_path = "/dev/tty.SLAB_USBtoUART";
+RPlidarDriver * openLidar(int &errorCode, const char * opt_com_path, bool debugInfo);
+void closeLidar(RPlidarDriver ** drv);
+bool readData(RPlidarDriver * drv, bool debugInfo = false);
+
